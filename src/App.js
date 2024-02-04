880a51df0ae35db017505825a59e54fbb004478e
Andrew Souvanlasy <andrewsouvanlasy@proton.me>
2024-02-04 12:13:55 -0500

    add and edit style and content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import './App.css';
-import Header from './components/Header/Header.js';
+import "./App.css";
+import Header from "./components/Header/Header.js";
 
 function App() {
   return (
-    <div className="app-body">
+    <div>
       <Header />
-      <h1>Findr Communities</h1>
       <main>
+        <h1>Findr - Find Local Communities</h1>
         <section id="about-section">
-          {/* Content for the About section */}
-        </section>
-        <section id="services-section">
-          {/* Content for the Services section */}
+          <p>
+            Findr is an app to connect people who enjoy the same topic to others
+            in the same geographical area. Imagine finding fellow book lovers,
+            gardeners, or DND players, all findable by city, town, or
+            neighbourhood.
+          </p>
+          <p>
+            Aside from connecting people, Findr also provides tools to organize
+            events, in-person meetups, and a budget tracker so people know how
+            much your community typically spends on activities.
+          </p>
+          <p>
+            <i>
+              App currently under construction. Findr will be released on the
+              App Store and Google Play soon.
+            </i>
+          </p>
         </section>
         <section id="contact-section">
-          {/* Content for the Contact section */}
+          <h2>Contact</h2>
+          <p>
+            Email us at{" "}
+            <a href="mailto:admin@findr.community">admin@findr.community</a> for
+            any enquiries.
+          </p>
         </section>
-        {/* Other sections/components */}
+        <section id="privacy-section">
+          <h2>Privacy</h2>
+          <p>
+            This is a social app, so Findr will need to collect some personal
+            and device data to function. These include your name, email,
+            location, and geolocation data. We will never sell your data to
+            third parties, and we will never use your data for any purpose other
+            than to provide the services offered by Findr.
+          </p>
+        </section>
+        <footer>
+          <hr />
+          <p>
+            <small>Made with ❤️ in Canada.</small>
+          </p>
+          <p>
+            <small>Findr - Find Local Communities 2024</small>
+          </p>
+        </footer>
       </main>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 // import logo from 'Assets/images/findr-logo-3.svg';
-import logo from '../../findr-logo.png'
-
+import logo from "../../findr-logo.png";
 
 function Header() {
   const scrollToSection = (sectionId) => {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <header className="site-header">
-        <div className="logo">
+      <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
       <nav className="navigation-menu">
         <ul>
-          <li onClick={() => scrollToSection('about-section')}>About</li>
-          <li onClick={() => scrollToSection('services-section')}>Services</li>
-          <li onClick={() => scrollToSection('contact-section')}>Contact</li>
+          <li onClick={() => scrollToSection("about-section")}>About</li>
+          <li onClick={() => scrollToSection("contact-section")}>Contact</li>
+          <li onClick={() => scrollToSection("privacy-section")}>Privacy</li>
         </ul>
       </nav>
-      <div style={{ clear: 'both' }}></div> {/* Clear float */}
+      <div style={{ clear: "both" }}></div> {/* Clear float */}
     </header>
   );
 }
