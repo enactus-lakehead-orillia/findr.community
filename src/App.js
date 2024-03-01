3baa040d18a424a6d431addb30d146ebebc40c42
dominik-merdzik <dominiktm@outlook.com>
2024-03-01 08:55:10 -0500

    Fixed plenty of styling - added coming soon images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header.js";
+import comingSoonIos from "../src/assets/images/coming-soon-ios.png"
+import comingSoonAndroid from "../src/assets/images/coming-soon-android.png"
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <main>
         <h1>Findr - Find Local Communities</h1>
         <section id="about-section" class="about-section">
+          <div>
           <p>
             Findr is an app to connect people who enjoy the same topic to others
             in the same geographical area. Imagine finding fellow book lovers,
@@ -25,17 +28,21 @@ function App() {
               App Store and Google Play soon.
             </i>
           </p>
+          <img src={comingSoonIos} alt="Coming soon to ios" class="comingSoon-img"/> 
+          <img src={comingSoonAndroid} alt="Coming soon to androidd" class="comingSoon-img"/> 
+          </div>
         </section>
+        <h2 class="contact-section-h2">Contact</h2>
         <section id="contact-section">
-          <h2>Contact</h2>
           <p>
             Email us at{" "}
             <a href="mailto:admin@findr.community">admin@findr.community</a> for
             any enquiries.
           </p>
         </section>
-        <section id="privacy-section">
-          <h2>Privacy</h2>
+        <h2 class="privacy-section-h2">Privacy</h2>
+        <section id="privacy-section" class="privacy-section">
+          <div>
           <p>
             This is a social app, so Findr will need to collect some personal
             and device data to function (when the app is fully released). These
@@ -48,6 +55,7 @@ function App() {
             If you would like us to delete your data, please{" "}
             <a href="mailto:admin@findr.community">contact us</a>.
           </p>
+          </div>
         </section>
         <footer>
           <hr />
