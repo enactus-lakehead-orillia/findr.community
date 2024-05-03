dc4bf57a0a4f7003d3efaab50f9049503efec0f1
Andrew Souvanlasy <andrewsouvanlasy@proton.me>
2024-05-03 14:11:42 -0400

    added navbar links which float left and right

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -2,25 +2,50 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/images/findr-logo.png";
 import SearchBar from "./SearchBar";
+import { StyleSheet } from "react-native";
 
 const Navigation = () => {
-return(
-    <div>
-        <nav>
-            <div className="container">
-                <div className="nav-left">
-                    <NavLink className= "logo">
-                    <img src={logo} alt="Logo" />
-                    </NavLink>
-                    <SearchBar/>
-
-
-                </div>
-
+    return (
+        <nav style={styles.navbar}>
+            <div style={styles.links}>
+                <NavLink to="/">
+                    <img src={logo} alt="Logo" style={styles.logo} />
+                </NavLink>
+                <SearchBar />
+            </div>
+            <div style={styles.links}>
+                <NavLink style={styles.link}>
+                    Login
+                </NavLink>
+                <NavLink style={styles.link}>
+                    Sign up
+                </NavLink>
             </div>
         </nav>
-    </div>
-);
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    navbar: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px",
+        backgroundColor: "#f2f2f2",
+    },
+    logo: {
+        marginRight: "10px",
+        width: "50px"
+    },
+    links: {
+        display: "flex",
+        alignItems: "center",
+    },
+    link: {
+        marginLeft: "10px",
+        textDecoration: "none",
+        color: "#333",
+    },
+});
 
 export default Navigation;
--- src/layout/SearchBar.js
+++ src/layout/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import {TextInput, TouchableOpacity} from "react-native";
-import {CiSearch} from "react-icons/ci";
+import { TextInput, TouchableOpacity } from "react-native";
+import { CiSearch } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
 
 const SearchBar = () => {
@@ -8,26 +8,27 @@ const SearchBar = () => {
 
     const clearSearch = () => {
         setSearchQuery("");
-      };
+    };
 
-return(
-    <div className="search-selection">
-        <CiSearch/>
-        <TextInput 
-        /** Input does not change when I try to modify the styling ***/
-            className = "input" 
-            placeholder="Search Communities and Events"
-            placeholderTextColor= "#000000"
-            value={searchQuery}
-            onChangeText={(text) => setSearchQuery(text)}
-        />
-        {searchQuery.length > 0 && (
-            <TouchableOpacity onPress={clearSearch}>
-                <IoMdClose/>
-            </TouchableOpacity>
-        )}
-    </div>
-);
+    return (
+        <div className="search-selection"> {/* <- CSS works on this element though */}
+            <CiSearch />
+            <TextInput
+                /* Input does not change when I try to modify the styling **/
+                /* It's not working cuz className isn't a property of TextInput -Andrew */
+                className="input"
+                placeholder="Search Communities and Events"
+                placeholderTextColor="#000000"
+                value={searchQuery}
+                onChangeText={(text) => setSearchQuery(text)}
+            />
+            {searchQuery.length > 0 && (
+                <TouchableOpacity onPress={clearSearch}>
+                    <IoMdClose />
+                </TouchableOpacity>
+            )}
+        </div>
+    );
 }
 
 export default SearchBar;
