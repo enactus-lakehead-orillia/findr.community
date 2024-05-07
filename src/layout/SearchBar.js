9c75927b15df551ecefaa3b708fb8f4765ac371b
aanisar <55286102+aanisar@users.noreply.github.com>
2024-05-07 17:11:31 -0400

    fixed the issues with searchbar

--- src/layout/SearchBar.js
+++ src/layout/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TextInput, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import { CiSearch } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
 
@@ -12,11 +12,8 @@ const SearchBar = () => {
 
     return (
         <div className="search-selection"> {/* <- CSS works on this element though */}
-            <CiSearch />
             <TextInput
-                /* Input does not change when I try to modify the styling **/
-                /* It's not working cuz className isn't a property of TextInput -Andrew */
-                className="input"
+                style={styles.input}
                 placeholder="Search Communities and Events"
                 placeholderTextColor="#000000"
                 value={searchQuery}
@@ -24,11 +21,31 @@ const SearchBar = () => {
             />
             {searchQuery.length > 0 && (
                 <TouchableOpacity onPress={clearSearch}>
-                    <IoMdClose />
+                    <IoMdClose className='lol'/>
                 </TouchableOpacity>
             )}
         </div>
     );
 }
+const styles = StyleSheet.create({
+    input: {
+    height: 40,
+    width: 250,
+    shadowColor: "#707070",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.9,
+    borderRadius: 25,
+    marginTop: 10,
+    marginBottom: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingLeft: 14,
+    paddingRight: 35,
+    borderColor: "#1f0000",
+    backgroundColor: "#F8F8F8",
+    },
+    
+  });
+  
 
 export default SearchBar;
